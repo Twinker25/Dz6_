aa00671d71ce5a03ee53a7d3ac42ad71f302d128
Twinker25 <shashokd@gmail.com>
2023-04-13 00:09:09 +0300

    Add Task2 firstly

--- Dz6_/Program.cs
+++ Dz6_/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice;
+            do
+            {
+                Console.Write("Enter task (1 - 4): ");
+                choice = int.Parse(Console.ReadLine()); 
+                switch (choice) 
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        Console.WriteLine("\nKettle:");
+                        Kettle kettle = new Kettle("", 0, 0);
+                        kettle.Input();
+                        kettle.Sound();
+                        kettle.Show();
+                        kettle.Desc();
+                        Console.WriteLine("\nMicrowave:");
+                        Microwave microwave = new Microwave("", 0, 0);
+                        microwave.Input();
+                        microwave.Sound();
+                        microwave.Show();
+                        microwave.Desc();
+                        Console.WriteLine("\nCar:");
+                        Car car = new Car("", 0, 0);
+                        car.Input();
+                        car.Sound();
+                        car.Show();
+                        car.Desc();
+                        Console.WriteLine("\nStreamer:");
+                        Streamer streamer = new Streamer("", 0, 0);
+                        streamer.Input();
+                        streamer.Sound();
+                        streamer.Show();
+                        streamer.Desc();
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default: Console.WriteLine("Error! Try again!"); break;
+                }
+            } while (choice < 1 || choice > 4);
         }
     }
 }
---
+++ Dz6_/Task2.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz6_
+{
+    class Device
+    {
+        public string name { get; set; }
+        public int year_made { get; set; }
+        public int money { get; set; }
+
+        public Device(string name, int year_made, int money)
+        {
+            this.name = name;
+            this.year_made = year_made;
+            this.money = money;
+        }
+    }
+
+    class Kettle : Device
+    {
+        public Kettle(string name, int year_made, int money) : base(name, year_made, money)
+        {
+        }
+        public void Input()
+        {
+            Console.Write("Enter name of device: ");
+            name = Console.ReadLine();
+            Console.Write("Enter year of made the device: ");
+            year_made = int.Parse(Console.ReadLine());
+            Console.Write("Enter value of device: ");
+            money = int.Parse(Console.ReadLine());
+        }
+        public void Sound()
+        {
+            Console.WriteLine("Sh - Sh!");
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {name}");
+        }
+        public void Desc()
+        {
+            Console.WriteLine($"Year of made: {year_made}");
+            Console.WriteLine($"Money: {money}");
+        }
+    }
+    class Microwave : Device
+    {
+        public Microwave(string name, int year_made, int money) : base(name, year_made, money)
+        {
+        }
+        public void Input()
+        {
+            Console.Write("Enter name of device: ");
+            name = Console.ReadLine();
+            Console.Write("Enter year of made the device: ");
+            year_made = int.Parse(Console.ReadLine());
+            Console.Write("Enter value of device: ");
+            money = int.Parse(Console.ReadLine());
+        }
+        public void Sound()
+        {
+            Console.WriteLine("Pip - Pip!");
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {name}");
+        }
+        public void Desc()
+        {
+            Console.WriteLine($"Year of made: {year_made}");
+            Console.WriteLine($"Money: {money}");
+        }
+    }
+    class Car : Device
+    {
+        public Car(string name, int year_made, int money) : base(name, year_made, money)
+        {
+        }
+        public void Input()
+        {
+            Console.Write("Enter name of device: ");
+            name = Console.ReadLine();
+            Console.Write("Enter year of made the device: ");
+            year_made = int.Parse(Console.ReadLine());
+            Console.Write("Enter value of device: ");
+            money = int.Parse(Console.ReadLine());
+        }
+        public void Sound()
+        {
+            Console.WriteLine("Bip - Bip!");
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {name}");
+        }
+        public void Desc()
+        {
+            Console.WriteLine($"Year of made: {year_made}");
+            Console.WriteLine($"Money: {money}");
+        }
+    }
+    class Streamer : Device
+    {
+        public Streamer(string name, int year_made, int money) : base(name, year_made, money)
+        {
+        }
+        public void Input()
+        {
+            Console.Write("Enter name of device: ");
+            name = Console.ReadLine();
+            Console.Write("Enter year of made the device: ");
+            year_made = int.Parse(Console.ReadLine());
+            Console.Write("Enter value of device: ");
+            money = int.Parse(Console.ReadLine());
+        }
+        public void Sound()
+        {
+            Console.WriteLine("Tu - Tu!");
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {name}");
+        }
+        public void Desc()
+        {
+            Console.WriteLine($"Year of made: {year_made}");
+            Console.WriteLine($"Money: {money}");
+        }
+    }
+}
